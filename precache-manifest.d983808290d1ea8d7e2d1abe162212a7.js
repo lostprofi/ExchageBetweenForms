8d60ccd2748b05b670ee1cf7f696d2214f2f5220
lostprofi <o-lazarchik@mail.ru>
2020-03-04 22:05:22 +0300

    Updates

---
+++ precache-manifest.d983808290d1ea8d7e2d1abe162212a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30608439a1ac3e49ccaf8de85a53e2b9",
+    "url": "/ExchageBetweenForms/index.html"
+  },
+  {
+    "revision": "a99923013358a2d98456",
+    "url": "/ExchageBetweenForms/static/css/main.e411bc88.chunk.css"
+  },
+  {
+    "revision": "1aaa137535dd4775e6d5",
+    "url": "/ExchageBetweenForms/static/js/2.5b0ccacb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ExchageBetweenForms/static/js/2.5b0ccacb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99923013358a2d98456",
+    "url": "/ExchageBetweenForms/static/js/main.1ff01cbb.chunk.js"
+  },
+  {
+    "revision": "8e7b6e7fb2fba9df46b1",
+    "url": "/ExchageBetweenForms/static/js/runtime-main.ab3cc644.js"
+  }
+]);
